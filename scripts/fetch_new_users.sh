6d3008dd384c0768c37e9cd05c984af0ecf35fed
Andrew Nisbet <anisbet@epl.ca>
2017-02-22 17:26:21 -0700

    Tested user load system script.

---
+++ scripts/fetch_new_users.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##################################################################################
+#
+# Bash shell script for starting and stopping the duplicate elastic search database. 
+#
+# Fetch the set of new users from the ILS, then zero out the file on success.
+#    Copyright (C) 2017  Andrew Nisbet
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Author:  Andrew Nisbet, Edmonton Public Library
+# Copyright (c) Wed Feb 22 16:51:17 MST 2017
+# Rev: 
+#          0.0 - Dev. 
+#
+##############################################################################
+### This script copies the new users' data from the ILS. The 
+# Setup variables
+SERVER=sirsi\@eplapp
+USER_FILE=users.lst
+REMOTE_DIR=/s/sirsi/Unicorn/EPLwork/cronjobscripts/OnlineRegistration
+LOCAL_DIR=/home/ilsadmin/duplicate_user/incoming
+PY_SCRIPT_DIR=/home/ilsadmin/duplicate_user/scripts/duplicate_user.py
+PY_SCRIPT_ARGS="-b$LOCAL_DIR/$USER_FILE"
+scp $SERVER:$REMOTE_DIR/$USER_FILE $LOCAL_DIR/$USER_FILE
+if [ -s "$LOCAL_DIR/$USER_FILE" ]; then
+	/usr/bin/python $PY_SCRIPT_DIR $PY_SCRIPT_ARGS
+	# Zero out the remote file to ensure we don't reload the users.
+	touch zero.file
+	scp zero.file $SERVER:$REMOTE_DIR/$USER_FILE
+	exit 0
+else
+	printf "* warn: file '%s' didn't copy over from %s.\n" $USER_FILE $SERVER >&2
+	exit 1
+fi
+# EOF
