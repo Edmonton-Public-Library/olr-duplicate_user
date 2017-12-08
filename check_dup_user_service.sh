322dd43b9b25dae84a1cda1a15adef9ca3d74b42
Andrew Nisbet <anisbet@epl.ca>
2017-12-08 15:24:27 -0700

    Now uses /etc/init.d/olr to start and stop on shutdown and restart.

--- check_dup_user_service.sh
+++ check_dup_user_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVICE=check_dup_user_service.sh
-is_running=$(ps aux | egrep elasticsearch | egrep -v egrep)
+is_running=$(ps aux | egrep elasticsearch | egrep -v grep)
 if [[ -z "${is_running// }" ]]; then
 	printf "* error: %s not running.\n" $SERVICE >&2
 	if ! /home/ilsadmin/duplicate_user/service.sh start; then
